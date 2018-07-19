0c2da9717ac3cdcf3a75407a03e99a000ff0b280
raunak2007 <league@iMac-2.attlocal.net>
2018-07-19 11:58:33 -0700

    day4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,12 +2,31 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+	public static void main(String[] args) {
+		Random random=new Random();
+		int randomNumber=random.nextInt(4);
+		System.out.println(randomNumber);
+		String awesome=JOptionPane.showInputDialog("Say something awesome");
+	if(randomNumber==0) {
+		JOptionPane.showMessageDialog(null, awesome+" is awesome");
+	}
+	if(randomNumber==1) {
+		JOptionPane.showMessageDialog(null, awesome+" is ok");
+	}
+	if(randomNumber==2) {
+		JOptionPane.showMessageDialog(null, awesome+" is boring");
+	}
+	if(randomNumber==3) {
+		JOptionPane.showMessageDialog(null,awesome+" will kill me");
+	}
+	}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	
 	// 3. Print out this variable
 
 	// 4. Get the user to enter something that they think is awesome
@@ -22,12 +41,11 @@ public class AwesomeOrNot {
 
 	// 7. If the random number is 2
 
-	// -- tell the user whatever they entered is boring.
+	}	// -- tell the user whatever they entered is boring.
 
 	// 8. If the random number is 3
 
 	// -- write your own answer
 
-}
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -5,23 +5,48 @@ import javax.swing.JOptionPane;
 public class QuizGame {
 
 	public static void main(String[] args) {
-		int score=0;
-		// 1.  Create a variable to hold the user's score 
-		String firstanswer =JOptionPane.showInputDialog("What year was Andrew Jackson was born");
-		if(firstanswer.equals("1767")){
-			score=score+5;
-			speak("You are correct. Your score is"+Integer.toString(score));
-			
+		int score = 0;
+		// 1. Create a variable to hold the user's score
+		String firstanswer = JOptionPane.showInputDialog("What year was Andrew Jackson was born");
+		if (firstanswer.equals("1767")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "You are correct. Your score is " + Integer.toString(score));
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. The correct answer is 1767");
 		}
+		String secondanswer = JOptionPane.showInputDialog("What year did Washington become a state");
+		if (secondanswer.equals("1889")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "You are correct. Your score is " + Integer.toString(score));
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. The correct answer is 1889.");
+		}
+		String thirdanswer = JOptionPane.showInputDialog("What was the fourth state admitted to the union");
+		if (thirdanswer.equals("Georgia") && thirdanswer.equals("georgia")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "You are correct. Your score is " + Integer.toString(score));
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. The correct answer is Georgia");
+		}
+		String fourthanswer = JOptionPane.showInputDialog("What war had the largest casualties on Americans");
+
+		if (fourthanswer.equals("The Civil War") && fourthanswer.equals("The American Civil War")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "You are correct. Your score is " + Integer.toString(score));
+
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Game over");
+		}
+		// 3. Use an if statement to check if their answer is correct
+
+		// 4. if the user's answer is correct
+
+		// -- add one to their score
+
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+		// 6. After all the questions have been asked, print the user's score
+
 	}
-	// 3. Use an if statement to check if their answer is correct
-
-	// 4. if the user's answer is correct
-
-	// -- add one to their score
-
-	// 5. Create more questions by repeating steps 1, 2, and 3 below.
-
-	// 6. After all the questions have been asked, print the user's score
-
 }
